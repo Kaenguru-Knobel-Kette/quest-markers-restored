1293a518d495ecf508786bbcce27d5be343793fc
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2019-04-26 23:31:48 +0200

    Add flags for bsa

--- Releases/build_full.py
+++ Releases/build_full.py
@@ -12,15 +12,21 @@ handler = logging.FileHandler("{}.log".format(release.__name__), "w")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 try:
+    flags = release.ArchiveFlags()
+    flags.check_misc = True
+    flags.check_retain_directory_names = True
+    flags.check_retain_file_names = True
     release.build_release(dir_src=config.DIR_REPO_LE,
                           temp_alt=config.DIR_TEMP_ALT,
                           arch_exe=config.ARCH_EXE_LE,
+                          arch_flags=flags,
                           trim_fomod=True,
                           warn_readmes=False)
     shutil.copytree(config.DIR_SCRIPTS_LE, config.DIR_SCRIPTS_SE)
     release.build_release(dir_src=config.DIR_REPO_SE,
                           temp_alt=config.DIR_TEMP_ALT,
                           arch_exe=config.ARCH_EXE_SE,
+                          arch_flags=flags,
                           trim_fomod=True,
                           warn_readmes=False)
     shutil.rmtree(config.DIR_SCRIPTS_SE)
