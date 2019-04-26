1491b28cc6120a80cb360e35c302e356dca3cda1
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2019-04-26 15:47:26 +0200

    Add build system

---
+++ Releases/build_full.py
@@ -0,0 +1,25 @@
+"""Build full release.
+Loose files are packed into a bsa and version numbers are added."""
+import config
+import logging
+import release
+
+logger = logging.getLogger(release.__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+handler = logging.FileHandler("{}.log".format(release.__name__), "w")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+try:
+    release.build_release(dir_src=config.DIR_REPO_LE,
+                          temp_alt=config.DIR_TEMP_ALT,
+                          arch_exe=config.ARCH_EXE_LE,
+                          trim_fomod=True,
+                          warn_readmes=False)
+    release.build_release(dir_src=config.DIR_REPO_SE,
+                          temp_alt=config.DIR_TEMP_ALT,
+                          arch_exe=config.ARCH_EXE_SE,
+                          trim_fomod=True,
+                          warn_readmes=False)
+except Exception as error:
+    logger.exception(error)
---
+++ Releases/config.py
@@ -0,0 +1,27 @@
+"""Global variables for Quest Markers Restored build system"""
+import os
+
+DIR_REPO = "C:\\Users\\user\\Documents\\GitHub\\quest-markers-restored"
+"""Directory where the git repository for Quest Markers Restored is stored."""
+
+DIR_REPO_LE = os.path.join(DIR_REPO, "Legendary Edition")
+"""Directory where all mod files for Legendary Edition are stored."""
+
+DIR_REPO_SE = os.path.join(DIR_REPO, "Special Edition")
+"""Directory where all mod files for Special Edition are stored."""
+
+DIR_TEMP_ALT = "C:\\Windows\\Temp"
+"""Directory where temporary files can be stored.
+The path must not contain a directory that ends with "Data"."""
+
+DIR_SKYRIM_LE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Skyrim"
+"""Directory where Skyrim Legendary Edition is installed."""
+
+DIR_SKYRIM_SE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\SkyrimSpecialEdition"
+"""Directory where Skyrim Special Edition is installed."""
+
+ARCH_EXE_LE = os.path.join(DIR_SKYRIM_LE, "Archive.exe")
+"""Path to Archive.exe for Legendary Edition"""
+
+ARCH_EXE_SE = os.path.join(DIR_SKYRIM_SE, "Tools\\Archive\\Archive.exe")
+"""Path to Archive.exe for Special Edition"""
